49c3c629d55fe3a5eb34cb20d737597bd06fff25
João Vitor Silva de Bitencourt <joaov.vbsb@gmail.com>
2021-05-01 15:41:24 -0300

    Nova requisição

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
-const express = require('express');
-const app = express();
-const PORTA = process.env.PORT||8080;
-const dados = require('./gerador_jogador.json');
+import express from 'express';
+import axios from 'axios';
 
-function gera_jogador(idade) {
-    var texto;
-    if (idade > 16) {
-        texto = "novato";
-    } if (idade > 21) {
-        texto = "profissional";
-    } if (idade > 28) {
-        texto_ = "veterano";
-    } if (idade > 34) {
-        texto = "master";
-    }
-    let ger_nome = Math.floor(Math.random() * (0 + 18) + 0);
-    let ger_sob = Math.floor(Math.random() * (0 + 28) + 0);
-    let ger_pos = Math.floor(Math.random() * (0 + 8) + 0);
-    let ger_clube = Math.floor(Math.random() * (0 + 20) + 0);
-    let mensagem_jogador = `${dados.nome[ger_nome]} ${dados.sobrenome[ger_sob]} é um futebolista brasileiro ${texto} que atua como ${dados.posicao[ger_pos]}. Atualmente defende o ${dados.clube[ger_clube]}.`;
-    return mensagem_jogador;
+const api = axios.create();
+const app = express();
+const PORTA = process.env.PORT || 8080;
+
+async function geraCarro() {
+    let resposta = await axios.get('http://fipeapi.appspot.com/api/1/carros/veiculos/21.json');
+    let data = resposta.data;
+    return data;
+}
+
+function geraID() {
+    let id = Math.floor(Math.random() * (0 + 18) + 0);
 }
 
 app.use('/gerador', function (req, res) {
-    var idade = req.query.filtro;
-    /*var objeto = {
-        nome: dados.nome[ger_nome],
-        sobrenome: dados.sobrenome[ger_sob],
-        idade: texto,
-        posicao: dados.posicao[ger_pos],
-        clube: dados.clube[ger_clube],
-        mensagem: mensagem_jogador
-
-    }*/
-    res.send(gera_jogador(idade));
+    let id = geraID();
+    let carro = geraCarro();
+    carro.then(resposta => {
+        resposta.forEach(element => {
+            if(element.id == id){
+                console.log(element.fipe_name);
+                res.send(element.fipe_name); 
+            }
+        });
+    })
 });
 
 app.listen(PORTA, () => {
